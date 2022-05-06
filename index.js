eac4cfde87266e5d5bff615d999450a82f821249
Azurene <98633770+Azurene@users.noreply.github.com>
2022-05-06 01:18:51 -0700

    initial commit and created initial questions to work on

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project? (Required)",
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a title for your project!');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: "input",
+        name: "description",
+        message: "Please describe your project (Required)",
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('Please enter a project description!');
+            }
+        }
+    },
+
+    {
+        
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+}
 
 // Function call to initialize app
 init();
