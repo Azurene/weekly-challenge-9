75839cf96c573ddefff783369e0c5162601f3205
Azurene <98633770+Azurene@users.noreply.github.com>
2022-05-08 22:31:05 -0700

    created generateMarkdown file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown')
+const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -76,22 +77,16 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    return fs.writeFile('./dist/README.md', fileName, data, err => {
-        if (err) {
-            reject(err);
-            return;
-        }
-
-        resolve({
-            ok: true,
-            message: 'File created!'
-        });
-    });
-};
+    return fs.writeFileSync(fileName, data);
+}
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
+    .then((data) => {
+            console.log("File Created!");
+            return writeToFile('./dist/README.md', generateMarkdown(data));
+    })
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,89 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "none") {
+    return "";
+  }
+
+  if (license === "Apache License 2.0") {
+    return `![License](https://img.shields.io/badge/license-Apache%202.0-blue)`;
+  }
+
+  if (license === "GNU GPLv3") {
+    return `![License](https://img.shields.io/badge/license-GNU%20GPLv3-blue)`
+  }
+
+  if (license === "MIT") {
+    return `![License](https://img.shields.io/badge/license-MIT-green)`
+  }
+
+  if (license === "ISC") {
+    return `![License](https://img.shields.io/badge/license-ISC-green)`
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "none") {
+    return "";
+  } else {
+    return `
+    * [License](#license)
+    `;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "none") {
+    return "";
+  } else {
+    return `
+### License
+    
+License used: ${license}
+    `
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${renderLicenseBadge(data.license)}
+
+  ### Description
+  ${data.description}
+
+  ### Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  ${renderLicenseLink(data.license)}
+  * [Contributing](#contributing)
+  * [Testing](#testing)
+  * [Contact](#contact)
+  
+  ### Installation
+  ${data.installation}
+
+  ### Usage
+  ${data.usage}
+
+  ${renderLicenseSection(data.license)}
+
+  ### Contributions
+  ${data.contributing}
+
+  ### Testing
+  To test, run the following command:
+  ${data.testing}
+
+  ### Contact
+  Github: [${data.github}](https://github.com/${data.github})
+
+  Email: ${data.email}
 `;
 }
 
